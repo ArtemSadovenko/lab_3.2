31a75c9b45d031a6ee0a8df915368c25d9f6c656
ArtemSadovenko <90398799+ArtemSadovenko@users.noreply.github.com>
2021-09-30 19:21:39 +0300

    Add files via upload

---
+++ lab_3.2/lab_3.2/lab_3.2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	double a;
+	double b;
+	double x;
+	double c;
+	double y;
+
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+	cout << "c = "; cin >> c;
+	cout << "x = "; cin >> x;
+	
+	if (x < 0 && b != 0) {
+		y = (a * x * x) - (b * x * x);
+	}
+	else if (x > 0 && b == 0) {
+		y = (x - a) / (x - c);
+	}
+	else {
+		y = (x + 5) / (c * (x - 10));
+	}
+
+	cout << "F = " << y;
+
+	return 0;
+}
+
+
